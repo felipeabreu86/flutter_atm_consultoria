3c5bcf607c6298a912f6d9a55aeda09061bf47d7
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-15 12:00:06 -0300

    Se aplicado, este commit irá criar o layout da tela Home do projeto

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -10,6 +10,66 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "ATM Consultoria",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10, top: 80),
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: null,
+                          child: Image.asset("assets/images/menu_empresa.png"),
+                        ),
+                        GestureDetector(
+                          onTap: null,
+                          child: Image.asset("assets/images/menu_servico.png"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: null,
+                        child: Image.asset("assets/images/menu_cliente.png"),
+                      ),
+                      GestureDetector(
+                        onTap: null,
+                        child: Image.asset("assets/images/menu_contato.png"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
